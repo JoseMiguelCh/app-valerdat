3a0978ee1bd302f13cc5cc3a241d1c1f44b9c9f3
Jose Miguel Chacón <josemiguelchacon@outlook.com>
2023-02-28 07:33:11 -0500

    Ex 2 - 3. Updated logest word method

--- DjangoBase/app/utils/wordfinder.py
+++ DjangoBase/app/utils/wordfinder.py
@@ -1,17 +1,22 @@
 import requests
 
 class WordFinder:
-    def __init__(self, url = '') -> None:
+    def __init__(self, source: str | list = '', type = 'external') -> None:
         """Initialize the WordFinder class.
         Args:
             url (str): The URL to the word list. Defaults to ''."""
         
         self.words: set = set()
-        if url == '':
-            url = 'https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english.txt'
-        response = requests.get(url)
-        for line in response.iter_lines():
-            self.words.add(line.decode('utf-8'))
+        response = ''
+        if source == '' and type == 'external':
+            source = 'https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english.txt'
+            response = requests.get(source)
+            for line in response.iter_lines():
+                self.words.add(line.decode('utf-8'))
+        else: 
+            for item in source:
+                self.words.add(item)
+        
                 
     def longest_word(self, letters):
         """Find the longest word that can be made from the given letters.
@@ -19,12 +24,10 @@ class WordFinder:
             letters (str): The letters to use.
         Returns:
             str: The longest word that can be made from the given letters."""
-        
-        letters = set(letters)
-        max_length = 0
-        max_word = ''
-        for word in self.words:
-            if set(word).issubset(letters) and len(word) > max_length:
-                max_length = len(word)
-                max_word = word
-        return max_word
+        def is_subset(word1, word2):
+            return all(word1.count(c) <= word2.count(c) for c in set(word1))
+
+        valid_words = [w for w in self.words if is_subset(w, letters)]
+        if not valid_words:
+            return None
+        return max(valid_words, key=len)
