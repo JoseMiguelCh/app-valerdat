1ade9a7dfefc927b8bed75e6af732acf26e2036f
Jose Miguel Chacón <josemiguelchacon@outlook.com>
2023-02-28 08:17:42 -0500

    Ex 3. Added basic test case

--- DjangoBase/app/tests.py
+++ DjangoBase/app/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from app.utils.wordfinder import WordFinder
 
-# Create your tests here.
+class WordFinderTestCase(TestCase):
+    def setUp(self):
+        self.finder = WordFinder()
+
+    def test_longest_word(self):
+        self.assertEqual(self.finder.longest_word('optonoceari'), 'cooperation')
